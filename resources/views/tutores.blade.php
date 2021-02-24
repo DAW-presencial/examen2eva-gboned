8a7566af4990f687ad17e66fc41526a8fd39c565
gboned <gboned@cifpfbmoll.eu>
2021-02-24 20:23:09 +0100

    Punto 5: Añadir required a campos necesarios

--- resources/views/tutores.blade.php
+++ resources/views/tutores.blade.php
@@ -12,11 +12,11 @@
         @csrf
         <div class="form-group">
             <label for="nombreEmpresa">{{__("messages.nombreEmpresa")}}</label>
-            <input type="text" name="companyName" class="form-control" placeholder="Introduce nombre" value="{{ old('nombreEmpresa') }}"><br>
+            <input required type="text" name="companyName" class="form-control" placeholder="Introduce nombre" value="{{ old('nombreEmpresa') }}"><br>
         </div>
         <div class="form-group">
             <label for="tipoDocumento">{{__("messages.tipoDocumento")}}</label>
-            <select class="form-control" id="tipoDocumento">
+            <select required class="form-control" id="tipoDocumento">
                 <option>Pasaporte</option>
                 <option>DNI</option>
                 <option>NIE</option>
@@ -24,19 +24,19 @@
         </div>
         <div class="form-group">
             <label for="numeroDocumento">{{__("messages.numeroDocumento")}}</label>
-            <input type="text" name="numeroDocumento" class="form-control" placeholder="Introduce Numero Documento" value="{{ old('numeroDocumento') }}"><br>
+            <input required type="text" name="numeroDocumento" class="form-control" placeholder="Introduce Numero Documento" value="{{ old('numeroDocumento') }}"><br>
         </div>
         <div class="form-group">
             <label for="nombre">{{__("messages.nombre")}}</label>
-            <input type="text" name="nombre" class="form-control" placeholder="Introduce Nombre" value="{{ old('nombre') }}"><br>
+            <input required type="text" name="nombre" class="form-control" placeholder="Introduce Nombre" value="{{ old('nombre') }}"><br>
         </div>
         <div class="form-group">
             <label for="primerApellido">{{__("messages.primerApellido")}}</label>
-            <input type="text" name="primerApellido" class="form-control" placeholder="Introduce Primer Apellido" value="{{ old('primerApellido') }}"><br>
+            <input required type="text" name="primerApellido" class="form-control" placeholder="Introduce Primer Apellido" value="{{ old('primerApellido') }}"><br>
         </div>            
         <div class="form-group">
             <label for="segundoApellido">{{__("messages.segundoApellido")}}</label>
-            <input type="text" name="segundoApellido" class="form-control" placeholder="Introduce Segundo Apellido" value="{{ old('segundoApellido') }}"><br>
+            <input required type="text" name="segundoApellido" class="form-control" placeholder="Introduce Segundo Apellido" value="{{ old('segundoApellido') }}"><br>
         </div> 
         <div class="form-group">
             <label for="paisDocumento">{{__("messages.paisDocumento")}}</label>
@@ -64,18 +64,18 @@
         </div>   
         <div class="form-group">
             <label for="estado">{{__("messages.estado")}}</label>
-            <select class="form-control" id="estado">
+            <select required class="form-control" id="estado">
                 <option>Activo</option>
                 <option>Inactivo</option>
             </select>
         </div>
         <div class="form-group">
             <label for="numeroTelefono">{{__("messages.numeroTelefono")}}</label>
-            <input type="tel" name="numeroTelefono" class="form-control" placeholder="Introduce Número Teléfono" value="{{ old('numeroTelefono') }}"><br>
+            <input required type="tel" name="numeroTelefono" class="form-control" placeholder="Introduce Número Teléfono" value="{{ old('numeroTelefono') }}"><br>
         </div>
         <div class="form-group">
             <label for="email">{{__("messages.email")}}</label>
-            <input type="mail" name="email" class="form-control" placeholder="Introduce Email" value="{{ old('email') }}"><br>
+            <input required type="mail" name="email" class="form-control" placeholder="Introduce Email" value="{{ old('email') }}"><br>
         </div>          
 
         <button type="submit" class="btn btn-primary">Enviar</button>
